aa240ef96c8b876a6d1e19eddd0ac2f1b3d5e441
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-04-08 16:34:54 +0200

    created the ul of 10 emails

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-const {createApp} = Vue
+const { createApp } = Vue
 
 createApp({
-    data(){
-        return{
+    data() {
+        return {
             mainApi: 'https://flynn.boolean.careers/exercises/api/',
             randomMails: [],
         }
     },
-    methods:{
-        getRandomMails(){
-            for(let i = 0; i<10; i++){
-            axios.get(`${this.mainApi}random/mail`).then((response)=>{
-                const mailArray= [];
-                // console.log(response.data); //check dei dati provenienti dall'API
+    methods: {
+        getRandomMails() {
+            const mailArray = [];
+            for (let i = 0; i < 10; i++) {
+                axios.get(`${this.mainApi}random/mail`).then((response) => {
+                    // console.log(response.data); //check dei dati provenienti dall'API
                     mailArray.push(response.data.response)
-                    this.randomMails = mailArray
-                    console.log(this.randomMails);
-            
-            })
-        }
+                    // console.log(this.randomMails);
+                    this.randomMails = [...mailArray]
+
+                })
+            }
         }
     },
-    created(){
-        
+    created() {
+
     },
-    mounted(){
+    mounted() {
         this.getRandomMails()
     }
 }).mount('#app')
