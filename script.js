46fc9cc38afdae8b8449a59e33f8cca8a1157e3e
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-04-08 15:59:24 +0200

    checked the data from api and fixed the directory of script.js

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ const {createApp} = Vue
 createApp({
     data(){
         return{
-            
+            mainApi: 'https://flynn.boolean.careers/exercises/api/',
         }
     },
     methods:{
-        
+        getRandomMails(){
+            axios.get(`${this.mainApi}random/mail`).then((response)=>{
+                console.log(response.data); //check dei dati provenienti dall'API
+            })
+            
+        }
     },
     created(){
         
     },
+    mounted(){
+        this.getRandomMails()
+    }
 }).mount('#app')
