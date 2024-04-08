d5ed6a509a4748aad45033038bac81089b60ceff
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-04-08 16:05:54 +0200

    printed in html the data from api

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ createApp({
     data(){
         return{
             mainApi: 'https://flynn.boolean.careers/exercises/api/',
+            randomMails: null,
         }
     },
     methods:{
         getRandomMails(){
             axios.get(`${this.mainApi}random/mail`).then((response)=>{
                 console.log(response.data); //check dei dati provenienti dall'API
+                this.randomMails = response.data.response
             })
             
         }
