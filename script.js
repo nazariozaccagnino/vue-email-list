70e75e2257ac620ac4d1577d1515614af060ac7c
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-04-08 16:21:33 +0200

    created the array with 10 various mails

--- script.js
+++ script.js
@@ -4,16 +4,21 @@ createApp({
     data(){
         return{
             mainApi: 'https://flynn.boolean.careers/exercises/api/',
-            randomMails: null,
+            randomMails: [],
         }
     },
     methods:{
         getRandomMails(){
+            for(let i = 0; i<10; i++){
             axios.get(`${this.mainApi}random/mail`).then((response)=>{
-                console.log(response.data); //check dei dati provenienti dall'API
-                this.randomMails = response.data.response
-            })
+                const mailArray= [];
+                // console.log(response.data); //check dei dati provenienti dall'API
+                    mailArray.push(response.data.response)
+                    this.randomMails = mailArray
+                    console.log(this.randomMails);
             
+            })
+        }
         }
     },
     created(){
